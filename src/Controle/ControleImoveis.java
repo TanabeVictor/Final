76acf2719cf17c600afc43248c643f916f11dc8d
José Francisco <jos.francisco.nascimento@gmail.com>
2017-06-28 11:39:33 -0300

    Criação do método setImovel

--- src/Controle/ControleImoveis.java
+++ src/Controle/ControleImoveis.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controle;
 
-/**
- *
- * @author vitau
- */
+import Entidades.Imovel;
+import java.util.*;
+
 public class ControleImoveis {
     
+    private Vector listaImovel = new Vector();
+    
+    //Inserção do imóvel
+    public void setImovel(int codigo, String tipo, String descricao, String nomeVendedor, String data, double preco) throws Exception {
+        Imovel objImovel = new Imovel(codigo, tipo, descricao, nomeVendedor, data, preco);
+        listaImovel.add(objImovel);
+    }
 }
