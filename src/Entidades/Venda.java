3dca2f9a5a960f0bf68659322a98d48359902714
José Francisco <jos.francisco.nascimento@gmail.com>
2017-06-28 12:21:19 -0300

    Criação dos construtores da entidade Venda

--- src/Entidades/Venda.java
+++ src/Entidades/Venda.java
@@ -1,17 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Entidades;
 
-/**
- *
- * @author vitau
- */
 public class Venda {
     private String nomeComprador, dataVenda;
     private double valorReal, valorAnunciado;
     private int nroCreci, codImovel;
+
+    public Venda(String nomeComprador, String dataVenda, double valorReal, double valorAnunciado, int nroCreci, int codImovel) {
+        setNomeComprador(nomeComprador);
+        setDataVenda(dataVenda);
+        setValorReal(valorReal);
+        setValorAnunciado(valorAnunciado);
+        setNroCreci(nroCreci);
+        setCodImovel(codImovel);
+    }
+    
+    
+
+    public String getNomeComprador() {
+        return nomeComprador;
+    }
+
+    public void setNomeComprador(String nomeComprador) {
+        this.nomeComprador = nomeComprador;
+    }
+
+    public String getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(String dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public double getValorReal() {
+        return valorReal;
+    }
+
+    public void setValorReal(double valorReal) {
+        this.valorReal = valorReal;
+    }
+
+    public double getValorAnunciado() {
+        return valorAnunciado;
+    }
+
+    public void setValorAnunciado(double valorAnunciado) {
+        this.valorAnunciado = valorAnunciado;
+    }
+
+    public int getNroCreci() {
+        return nroCreci;
+    }
+
+    public void setNroCreci(int nroCreci) {
+        this.nroCreci = nroCreci;
+    }
+
+    public int getCodImovel() {
+        return codImovel;
+    }
+
+    public void setCodImovel(int codImovel) {
+        this.codImovel = codImovel;
+    }
+    
+    
     
 }
